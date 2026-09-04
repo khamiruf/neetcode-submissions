5288da284be192c45b6c15d79a0b8c0a709d91ad
Khairul Amiruf <khairul.amiruf@gmail.com>
2026-09-04 11:36:09 +0800

    Add: lru-cache - submission-4

---
+++ Data Structures & Algorithms/lru-cache/submission-4.py
@@ -0,0 +1,58 @@
+class Node:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+    #       next --> 
+    # LEFT        node      RIGHT
+    #               <-- prev
+
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.cache = {} # map key to node
+        
+        # left = LRU, right = MRU
+        self.left, self.right = Node(0,0), Node(0,0)
+        self.left.next = self.right
+        self.right.prev = self.left
+    
+    # remove node from list
+    def remove(self, node):
+        prev = node.prev
+        nxt = node.next
+        prev.next = nxt
+        nxt.prev = prev
+    
+    # Insert at Right
+    def insert(self, node):
+        prev = self.right.prev
+        nxt = self.right
+
+        prev.next = node
+        nxt.prev = node
+        node.prev = prev
+        node.next = nxt
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            # need to update to the most recent
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
+        return -1        
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key,value)
+        self.insert(self.cache[key])
+
+        # need to check for capacity
+        if len(self.cache) > self.cap:
+            # remove from lru and delete the LRU from hashmap
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
